173524eace5cc85172a890a32afbe46fb978db59
ecorina <serban.corrina@gmail.com>
2023-08-31 14:53:20 +0100

    clean code

--- devices.py
+++
@@ -1,6 +0,0 @@
-class Device:
-    def __init__(self, ip, device_type, username, password):
-        self.ip = ip
-        self.device_type = device_type
-        self.username = username
-        self.password = password
