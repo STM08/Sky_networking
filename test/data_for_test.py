4146101d3c329e2b444b5b95bf72544c0039b35e
Justin Ho <hochungyan@live.hk>
2023-09-04 17:04:52 +0100

    Unit Test:Create Loopback

--- test/data_for_test.py
+++ test/data_for_test.py
@@ -11,3 +11,29 @@ test_device = MockDevice(
     username="developer",
     password="lastorangerestoreball8876"
 )
+
+mock_request = {
+    "name": "1",
+    "description": "Loopback created via netconfffffffff",
+    "ip": "10.0.0.8",
+    "netmask": "255.255.255.0",
+    "dry-run": False
+}
+
+mock_config_response = """<?xml version="1.0" ?>
+<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+    <interface>
+        <name>Loopback1</name>
+        <description>Loopback created via netconfffffffff</description>
+        <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:softwareLoopback</type>
+        <enabled>true</enabled>
+        <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
+            <address>
+                <ip>10.0.0.8</ip>
+                <netmask>255.255.255.0</netmask>
+            </address>
+        </ipv4>
+    </interface>
+</interfaces>
+"""
+
--- test/test_check.py
+++ test/test_check.py
@@ -1,22 +1,63 @@
 import pytest
 from app.api.connect import netmiko_connection, ncclient_connection
-from .data_for_test import test_device
+from .data_for_test import test_device, mock_request, mock_config_response
 from app.api.create_loopback import create_loopback
 from unittest.mock import Mock
 
-
-## Test for connections
-def test_connetion():
-    pass
-def test_disconnection():
-    pass
-
-## loopbacks
+## Test for Sandbox Connection
 def test_connect_device():
     try:
         connection = netmiko_connection(test_device)
         assert connection is not None, "Failed to establish connection"
     except ConnectionError as e:
         assert False, str(e)
-        
 
+def test_create_loopback():
+    # Mock the connection object
+    mock_connection = Mock()
+    mock_connection.edit_config.return_value = "<ok/>"  # Mocking a successful NETCONF response
+    mock_connection.get_config.return_value = mock_config_response
+
+    # Call the create_loopback function
+    response = create_loopback(mock_connection, mock_request)
+
+    # Define the template and expected response
+    template = """Successfully configured {loopback_name}
+
+<?xml version="1.0" ?>
+<interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+
+      <interface>
+
+              <name>{loopback_name}</name>
+
+              <description>Loopback created via netconfffffffff</description>
+
+              <type xmlns:ianaift="urn:ietf:params:xml:ns:yang:iana-if-type">ianaift:softwareLoopback</type>
+
+              <enabled>true</enabled>
+
+              <ipv4 xmlns="urn:ietf:params:xml:ns:yang:ietf-ip">
+
+                      <address>
+
+                              <ip>{ip_address}</ip>
+
+                              <netmask>255.255.255.0</netmask>
+
+                      </address>
+
+              </ipv4>
+
+      </interface>
+
+</interfaces>
+"""
+
+    expected_response = template.format(loopback_name="Loopback1", ip_address="10.0.0.8")
+    
+    # Remove extra spaces and line breaks for comparison
+    cleaned_response = '\n'.join([line.strip() for line in response.splitlines() if line.strip()])
+    cleaned_expected_response = '\n'.join([line.strip() for line in expected_response.splitlines() if line.strip()])
+    
+    assert cleaned_response == cleaned_expected_response, f"Expected: {cleaned_expected_response}, but got: {cleaned_response}"
